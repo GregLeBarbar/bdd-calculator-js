d4a8c26e40a2c85a1fa0f9ebfe45dba90fd9f03e
Grégoy Charmier <gregory.charmier@gmail.com>
2019-01-22 06:58:20 +0100

    Improve init addition_steps

--- features/step_definitions/addition_steps.js
+++ features/step_definitions/addition_steps.js
@@ -1,18 +1,18 @@
 const { Given, When, Then } = require('cucumber')
 
 
-Given('the numbers {int} and {int}', function (int, int2, callback) {
+Given('the numbers {int} and {int}', function (int, int2) {
     // Write code here that turns the phrase above into concrete actions
-    callback(null, 'pending');
+    return 'pending';
 });
 
 
-When('they are added together', function (callback) {
+When('they are added together', function () {
     // Write code here that turns the phrase above into concrete actions
-    callback(null, 'pending');
+    return 'pending';
 });
 
-Then('should the result be {int}', function (int, callback) {
+Then('should the result be {int}', function (int) {
     // Write code here that turns the phrase above into concrete actions
-    callback(null, 'pending');
+    return 'pending';
 });
